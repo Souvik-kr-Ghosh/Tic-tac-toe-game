6724f397b6cb29b30f479b7a2f75b951631b143b
Souvik Ghosh <149665253+Souvik-kr-Ghosh@users.noreply.github.com>
2025-05-13 00:24:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-console.log("Welcome to tic tac toe")
-let music=new Audio("music.mp3")
-let audioturn=new Audio("ting.mp3")
-let gameover=new Audio("gameover.mp3")
-let turn="X"
+console.log("Welcome to tic tac toe");
+
+let music = new Audio("music.mp3");
+let audioturn = new Audio("ting.mp3");
+let gameover = new Audio("gameover.mp3");
+
+let turn = "X";
 let isgameover = false;
+
 // Function to change the turn
-const changeturn = ()=>{
-    return turn === "X"?"0":"X"
-}
+const changeturn = () => {
+    return turn === "X" ? "0" : "X";
+};
+
 // Function to check for a win
-const checkWin = ()=>{
+const checkWin = () => {
     let boxtext = document.getElementsByClassName('boxtext');
     let wins = [
         [0, 1, 2, 5, 5, 0],
@@ -20,43 +24,49 @@ const checkWin = ()=>{
         [2, 5, 8, 15, 15, 90],
         [0, 4, 8, 5, 15, 45],
         [2, 4, 6, 5, 15, 135],
-    ]
-    wins.forEach(e =>{
-        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
-            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
-            isgameover = true
+    ];
+    wins.forEach(e => {
+        if (
+            boxtext[e[0]].innerText === boxtext[e[1]].innerText &&
+            boxtext[e[2]].innerText === boxtext[e[1]].innerText &&
+            boxtext[e[0]].innerText !== ""
+        ) {
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won";
+            isgameover = true;
+            gameover.play();
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
-            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
             document.querySelector(".line").style.width = "20vw";
         }
-    })  
-}
-//Game Logic
-let boxes=document.getElementsByClassName("box");
-Array.from(boxes).forEach(element =>{
-    let boxtext=element.querySelector('.boxtext');
-    element.addEventListener('click', ()=>{
-        if(boxtext.innerText==='')
-        {
-            boxtext.innerText=turn;
-            turn=changeturn();
+    });
+};
+
+// Game Logic
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', () => {
+        if (boxtext.innerText === '') {
+            boxtext.innerText = turn;
+            turn = changeturn();
             audioturn.play();
             checkWin();
-            if(!isgameover){
-            document.getElementsByClassName("info")[0].innerText="Turn for" +turn;
+            if (!isgameover) {
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
             }
         }
-    })
-})
-// Add onclick listener to reset
-reset.addEventListener('click',()=>{
+    });
+});
+
+// ✅ Fix: Properly select the reset button
+document.getElementById('reset').addEventListener('click', () => {
     let boxtexts = document.querySelectorAll('.boxtext');
     Array.from(boxtexts).forEach(element => {
-        element.innerText = ""
+        element.innerText = "";
+    });
+    turn = "X";
+    isgameover = false;
+    document.querySelector(".line").style.width = "0vw";
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
 });
-turn = "X"; 
-isgameover = false
-document.querySelector(".line").style.width = "0vw";
-document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"   
-})
